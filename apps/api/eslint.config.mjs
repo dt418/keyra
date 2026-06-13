561933a096f76f0f6bc8f38a3f7fdce4f82881ef
thanhd <danhthanh418@gmail.com>
2026-06-14 00:20:53 +0700

    fix: add eslint config for v9

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,26 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/.wrangler/**'],
+  },
+];
